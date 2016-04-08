865b1526a21ad0563e55946193d3d6f6d26357c2
Eli Brown <ebrown6@uab.edu>
2016-04-08 00:59:00 -0500

    Day 1

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,4 +1,48 @@
-var app = angular.module('App', ['ngCordova']);
+var app = angular.module('App', ['ngCordova',
+'ngMaterial',
+'ngRoute',
+'uiGmapgoogle-maps']);
 
 app.controller('appCtrl', function($scope) {
 });
+app.controller('locCtrl', function($scope) {
+  $scope.onlyZIP = /^\d{5}(?:[-\s]\d{4})?$/;
+  $scope.zipcode = null
+  uiGmapGoogleMapApi.then(function(maps) {
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+   });
+});
+
+app.controller('factsCtrl', function($scope) {
+});
+app.controller('weatherCtrl', function($scope, $http) {
+
+});
+
+app.config(function(uiGmapGoogleMapApi) {
+    uiGmapGoogleMapApi.configure({
+        key: 'AIzaSyCIFs1cZyZmVf_OAgoxfjRW5P-4CsQmtq0',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/location', {
+        templateUrl: 'views/location.html',
+        controller: 'locCtrl'
+      })
+      .when('/weather', {
+        templateUrl: 'views/weather.html',
+        controller: 'weatherCtrl'
+      })
+
+      .when('/facts', {
+        templateUrl: 'views/facts.html',
+        controller: 'factsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/location'
+        });
+  }]);
--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,46 @@
-var app = angular.module('App', ['ngCordova']);
+var app = angular.module('App', ['ngCordova',
+'ngMaterial',
+'ngRoute',
+'uiGmapgoogle-maps']);
 
 app.controller('appCtrl', function($scope) {
 });
+app.controller('locCtrl', function($scope) {
+  $scope.onlyZIP = /^\d{5}(?:[-\s]\d{4})?$/;
+  $scope.zipcode = null
+  uiGmapGoogleMapApi.then(function(maps) {
+
+   });
+});
+
+app.controller('factsCtrl', function($scope) {
+});
+app.controller('weatherCtrl', function($scope, $http) {
+
+});
+
+app.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/location', {
+        templateUrl: 'views/location.html',
+        controller: 'locCtrl'
+      })
+      .when('/weather', {
+        templateUrl: 'views/weather.html',
+        controller: 'weatherCtrl'
+      })
+
+      .when('/facts', {
+        templateUrl: 'views/facts.html',
+        controller: 'factsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/location'
+        });
+  }]);
